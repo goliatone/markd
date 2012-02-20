1068128b86b7fdad7c31cb40780b3e2b1d6ff9be
Matt Walters <mwalters8@gmail.com>
2012-02-20 18:27:50 -0500

    Remove dropbox link from sidebar and add about.me link

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -2,15 +2,13 @@
 			<div class="span4">
 				<h3>Connect</h3>
 				<div class="side-buttons">
+					<a target="_blank" href="http://about.me/mattwalters"><img src="/images/me_32.png" alt="Matt Walters on about.me" /></a>
 					<a target="_blank" href="http://twitter.com/mwalters"><img src="/images/twitter_32.png" alt="Matt Walters on twitter" /></a>
 					<a target="_blank" href="http://www.linkedin.com/in/mattswalters"><img src="/images/linkedin_32.png" alt="Matt Walters on Linked In" /></a>
 					<a target="_blank" href="https://github.com/mwalters"><img src="/images/github_32.png" alt="Matt Walters on github" /></a>
 					<a target="_blank" href="/rss.xml"><img src="/images/rss_32.png" alt="RSS Feed for MattWalters.net" /></a>
 				</div>
 				<?php $hooks->execute_actions('markd_sidebar'); ?>
-				<br/>
-				<br/>
-				<center><a href="http://db.tt/vIzxb0uf" title="Sign up for a free dropbox account"><img src="/images/sm-banner-dropbox.png" alt="Free Dropbox Account" /></a></center>
 			</div> <!-- span4 -->
 		</div> <!-- row -->
 		<div class="row">
